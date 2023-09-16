ee6975bc287644bc40f8052773d24696c2855989
moldovexc <alexanderparpulansky@gmail.com>
2023-09-16 12:05:14 +0300

    Refactoring validation messages

--- src/main/java/com/example/googleauthenticationspring/utils/Messages.java
+++ src/main/java/com/example/googleauthenticationspring/utils/Messages.java
@@ -8,7 +8,7 @@ public class Messages {
     public static final String USERNAME_EXISTS = "Username already exists!";
     public static final String EMAIL_EXISTS = "Email already exists!";
     public static final String INVALID_JWT_TOKEN = "{\"message\": \"Invalid JWT Token\"}";
-    public static final String EXPIRED_JWT_TOKEN = "{\"message\": \"Expired JWT Token\"}";
+    public static final String EXPIRED_JWT_TOKEN = "{\"message\": \"Session expired. You have been signed out\"}";
     public static final String UNSUPPORTED_JWT_TOKEN = "{\"message\": \"Unsupported JWT Token\"}";
     public static final String JWT_CLAIM_EMPTY = "{\"message\": \"JWT claim string is empty\"}";;
     public static final String INCORRECT_CREDENTIALS = "Incorrect email or password. Please try again.";
